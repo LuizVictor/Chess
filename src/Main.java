87fa5387f366b5287e485f6cfd94ad074cf5241b
Luiz Victor <luizvictor.dev@outlook.com>
2023-09-22 18:43:23 -0300

    creating class Position

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import BoardGame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+        Position position = new Position(3, 5);
+
+        System.out.println(position);
+    }
+}
