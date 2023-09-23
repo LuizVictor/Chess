88cc7e4f75a902623d6fa2ba117b8da44d3b6e67
Luiz Victor <luizvictor.dev@outlook.com>
2023-09-23 13:23:07 -0300

    implementing possible Rook movements

--- src/Chess/Pieces/Rook.java
+++ src/Chess/Pieces/Rook.java
@@ -1,6 +1,7 @@
 package Chess.Pieces;
 
 import BoardGame.Board;
+import BoardGame.Position;
 import Chess.ChessPiece;
 import Chess.Color;
 
@@ -17,6 +18,31 @@ public class Rook extends ChessPiece {
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] matrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        makeMove(matrix, -1, 0); // up
+        makeMove(matrix, 1, 0);  // down
+        makeMove(matrix, 0, -1); // left
+        makeMove(matrix, 0, 1);  // right
+
         return matrix;
+    }
+
+    private void makeMove(boolean[][] matrix, int row, int column) {
+        Position currentPosition = new Position(
+                position.getRow() + row,
+                position.getColumn() + column
+        );
+
+        while (getBoard().positionExists(currentPosition) && !getBoard().thereIsAPiece(currentPosition)) {
+            matrix[currentPosition.getRow()][currentPosition.getColumn()] = true;
+            currentPosition.setValues(
+                    currentPosition.getRow() + row,
+                    currentPosition.getColumn() + column
+            );
+        }
+
+        if (getBoard().positionExists(currentPosition) && isThereOpponentPiece(currentPosition)) {
+            matrix[currentPosition.getRow()][position.getColumn()] = true;
+        }
     }
 }
